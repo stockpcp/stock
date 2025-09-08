9de6c387f5f341fda5a3ffb5e06fed6500004376
Plywood Inventory <227446693+stockpcp@users.noreply.github.com>
2025-09-08 16:09:20 -0300

    script.js

--- script.js
+++ script.js
@@ -349,10 +349,10 @@ function showProductDetails(product) {
                 </div>
             </div>
             <div class="modal-section">
-                <h4>📦 Stock & Weight Information</h4>
+                <h4>📦 Stock & Weight Info</h4>
                 <div class="section-grid">
                     <div class="detail-item">
-                        <div class="detail-label">available</div>
+                        <div class="detail-label">Crates available</div>
                         <div class="detail-value">${product.Crates} crates</div>
                     </div>
                     <div class="detail-item">
